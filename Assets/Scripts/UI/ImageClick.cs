c21c20a060fd37e21be8f5ec8e0e008f7a061ecf
Fernando Tarango <fertarvega99@gmail.com>
2023-02-21 18:21:12 -0600

    One unit per grid square

--- Assets/Scripts/UI/ImageClick.cs
+++ Assets/Scripts/UI/ImageClick.cs
@@ -8,6 +8,7 @@ public class ImageClick : MonoBehaviour, IPointerDownHandler, IPointerUpHandler,
     public GameObject enemyPrefab;
     private Camera mainCamera;
     private bool isHovering = false;
+    [SerializeField] private Unit unit;
 
     void Start(){
         mainCamera = Camera.main;
@@ -38,18 +39,20 @@ public class ImageClick : MonoBehaviour, IPointerDownHandler, IPointerUpHandler,
         RaycastHit hit;
 
         // Cast the ray and check if it hits an object
-        if (Physics.Raycast(ray, out hit))
-        {
-            Vector3 worldPos = LevelGrid.Instance.GetWorldPosition(LevelGrid.Instance.GetGridPosition(hit.point));
-            // Print the world position of the mouse
-            // Debug.Log("Mouse position: " + worldPos);
-            Instantiate(enemyPrefab, worldPos, Quaternion.identity);
+        if (Physics.Raycast(ray, out hit)){
+            GridPosition gridPosition = LevelGrid.Instance.GetGridPosition(hit.point);
+            if(LevelGrid.Instance.IsValidGridPosition(gridPosition) && !LevelGrid.Instance.HasAnyUnitOnGridPosition(gridPosition)){
+                Vector3 worldPos = LevelGrid.Instance.GetWorldPosition(gridPosition);
+                // Print the world position of the mouse
+                // Debug.Log("Mouse position: " + worldPos);
+                Unit newUnit = Instantiate(unit, worldPos, Quaternion.identity);
+                LevelGrid.Instance.AddUnitAtGridPosition(gridPosition, newUnit);
+            }
         }
         
     }
     
-    private void Update()
-    {
-        
+    private void Update(){
+
     }
 }
