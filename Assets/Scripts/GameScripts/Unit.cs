26b78eac0035abe3f6822ff24063bca5898e89e9
Fernando Tarango <fertarvega99@gmail.com>
2023-02-24 11:19:41 -0600

    Unit separation

--- Assets/Scripts/GameScripts/Unit.cs
+++ Assets/Scripts/GameScripts/Unit.cs
@@ -4,19 +4,27 @@ using UnityEngine;
 
 public class Unit : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private GridPosition gridPosition;
+    private TowerScript tower;
+
+    private void Start()
     {
+
         UnitSelections.Instance.unitList.Add(this.gameObject);
+        gridPosition = LevelGrid.Instance.GetGridPosition(transform.position);
+        LevelGrid.Instance.AddUnitAtGridPosition(gridPosition, this);
+        tower = GetComponent<TowerScript>();
     }
 
     private void OnDestroy() {
         UnitSelections.Instance.unitList.Remove(this.gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public GridPosition GetGridPosition(){
+        return gridPosition;
+    }
+    // Here need to be the scripts depending on what kind of unit is
+    public TowerScript GetTower(){
+        return tower;
     }
 }
